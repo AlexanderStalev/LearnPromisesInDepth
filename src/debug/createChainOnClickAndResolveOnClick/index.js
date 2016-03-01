fcbe91f27c02cca5eba5196429ed0499d0c4894f
Alex <alexander.alexeev@outlook.com>
2016-03-01 14:34:06 +0300

    Создание цепочки и resolve по onClick.

--- src/debug/createChainOnClickAndResolveOnClick/index.js
+++ src/debug/createChainOnClickAndResolveOnClick/index.js
@@ -1,11 +1,29 @@
 angular
     .module('myApp', [])
     .controller('myController', function ($scope, $q) {
-        $scope.create = function () {
+        var deferred = $q.defer();
 
+        $scope.create = function () {
+            deferred.promise
+                .then(function (params) {
+                    return $q(function (resolve) {
+                        setTimeout(function () {
+                            resolve(params + 'b');
+                        }, 200);
+                    });
+                })
+                .then(function (params) {
+                    return params + 'c';
+                })
+                .then(function (params) {
+                    throw new Error(params + 'd');
+                })
+                .catch(function (error) {
+                    console.log(error.message + 'e');
+                });
         }
 
         $scope.resolve = function () {
-
+            deferred && deferred.resolve('a');
         }
     });
