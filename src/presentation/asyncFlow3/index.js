7465a3701d114666d657d8fff75901c4c86b74af
Alex <alexander.alexeev@outlook.com>
2016-04-05 00:46:40 +0300

    Состояния промиса.

--- src/presentation/asyncFlow3/index.js
+++ src/presentation/asyncFlow3/index.js
@@ -1,15 +1,26 @@
 angular
     .module('myApp', [])
     .controller('myController', function ($scope, $q) {
-        var promise = $q.resolve();
-        console.log(promise.$$state.status === 1); // fulfilled.
+        var defer1 = $q.defer(),
+            promise1 = defer1.promise,
+            promise2,
+            promise3,
+            promise4;
 
-        var promise2 = promise.then(function() {});
-        console.log(promise2.$$state.status === 0); // pending.
-
-        $scope.$evalAsync(function() {
-            console.log(promise2.$$state.status === 1); // fulfilled.
+        promise2 = promise1.then(function (params) {
+            console.log(1);
         });
+        promise3 = promise2.then(function (params) {
+            console.log(2);
+        });
+        promise4 = promise3.then(function (params) {
+            console.log(2);
+        });
+
+
+
+
+       
     });
 
 //$scope.$evalAsync(function () {
